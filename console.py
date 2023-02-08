e02a8d1e4bafd65315c064284a2c58359feaf12e
phemsie <femi_oworu@yahoo.com>
2023-02-08 11:16:28 +0100

    AirBnB_clone

--- console.py
+++ console.py
@@ -1,20 +1,3 @@
-
-AgeseVictor
-/
-AirBnB_clone
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-AirBnB_clone/console.py
-@AgeseVictor
-AgeseVictor grace
- 1 contributor
-Executable File  257 lines (234 sloc)  7.87 KB
 #!/usr/bin/python3
 import cmd
 import json
