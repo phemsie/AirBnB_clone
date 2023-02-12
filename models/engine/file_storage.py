600ea7987f9f510e9a65af0f63fd3feecf045102
phemsie <femi_oworu@yahoo.com>
2023-02-12 17:02:06 +0100

    AirBnB_clone

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,152 +1,95 @@
 #!/usr/bin/python3
-""" A module that contains a class `FileStorage` that serializes
-instances to a JSON file and deserializes a JSON file to instances:
-file_storage.py -
-"""
+"""Module for FileStorage class."""
+import datetime
 import json
-from datetime import datetime
+import os
 
 
-# Task 5:
-class FileStorage():
-    """A class that serialies instances to a JSON file and
-    deserializes a JSON file to instances:
-    """
-    # file_path: A string - path to the JSON file (ex: file.json)
+class FileStorage:
+
+    """Class for storing and retrieving data"""
     __file_path = "file.json"
-
-    # objects: A dictionary - that but will store all objects by:
-    # <class name>.id (ex: to store a BaseModel object with id=12121212,
-    # the key will be BaseModel.12121212)
-    __objects = dict()
-
-    def __init__(self):
-        pass
+    __objects = {}
 
     def all(self):
-        """Returns the dictionary __objects
-        """
-        # Retrieve previously saved data
-        type(self).reload(self)
-
-        new_dict = dict()
-
-        temp = type(self).__objects
-        if len(temp) > 0:
-            for key in temp.keys():
-                tmp_list = key.split('.')
-                new_dict[key] = "[{}] ({}) {}".format(tmp_list[0],
-                                                      tmp_list[1],
-                                                      temp[key])
-        return new_dict
+        """returns the dictionary __objects"""
+        return FileStorage.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id
-        """
-        obj_class_name = type(obj).__name__
-
-        # If the attribute `id` is not set, this line below should
-        # raise AttributeError
-        obj_id = str(getattr(obj, "id"))
-
-        # merge class name and id
-        new_key = obj_class_name + "." + obj_id
-
-        # Update dictionary `__objects`
-        type(self).__objects[new_key] = obj.__dict__
+        """sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file (path: __file_path)
-        """
-        FILE_PATH = type(self).__file_path
+        """ serializes __objects to the JSON file (path: __file_path)"""
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+            json.dump(d, f)
 
-        # temporary dictionary
-        new_dict = self.__objects.copy()
+    def classes(self):
+        """Returns a dictionary of valid classes and their references"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
 
-        if len(new_dict) != 0:  # __objects isn't empty
-            for key in new_dict.keys():
-                new_dict[key] = type(self).to_dict(new_dict[key])
-                tmp_list = key.split(sep='.')
-                new_dict[key]["__class__"] = tmp_list[0]
-
-        # Write json string to file
-        with open(FILE_PATH, mode="w", encoding="utf-8") as fhand:
-            json.dump(new_dict, fhand)
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
 
     def reload(self):
-        """ Deserializes the JSON file to __objects (only if the JSON
-        file (__file_path) exists ; otherwise, do nothing. If the
-        file doesn't exist, no exception should be raised)
-        """
-        FILE_PATH = type(self).__file_path
+        """Reloads the stored objects"""
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+            obj_dict = json.load(f)
+            obj_dict = {k: self.classes()[v["__class__"]](**v)
+                        for k, v in obj_dict.items()}
+            # TODO: should this overwrite or insert?
+            FileStorage.__objects = obj_dict
 
-        try:
-            with open(FILE_PATH, encoding="utf-8") as fhand:
-                temp = json.load(fhand)  # Store json string in temp
-        except FileNotFoundError:
-            pass  # File does not exist - do nothing
-        else:
-            new_dict = dict()
-            for key in temp.keys():
-                new_dict[key] = type(self).from_dict(temp[key])
-
-            type(self).__objects = new_dict
-
-    @staticmethod
-    def to_dict(obj):
-        """Serializes datetime objects to JSON
-        """
-        new_dict = obj.copy()
-
-        new_dict["created_at"] = new_dict["created_at"].strftime(
-            "%Y-%m-%dT%H:%M:%S.%f")
-        new_dict["updated_at"] = new_dict["updated_at"].strftime(
-            "%Y-%m-%dT%H:%M:%S.%f")
-
-        return new_dict
-
-    @staticmethod
-    def from_dict(obj):
-        """Deserializes JSON object to datetime object
-        """
-        new_dict = obj.copy()
-
-        new_dict["created_at"] = datetime.strptime(new_dict["created_at"],
-                                                   "%Y-%m-%dT%H:%M:%S.%f")
-        new_dict["updated_at"] = datetime.strptime(new_dict["updated_at"],
-                                                   "%Y-%m-%dT%H:%M:%S.%f")
-
-        if hasattr(new_dict, "__class__"):
-            del new_dict["__class__"]
-
-        return new_dict
-
-    def destroy(self, class_name, obj_id):
-        """A function that deletes an instance based on its
-        class name and id
-        """
-        for key in type(self).__objects.keys():
-            if key == class_name + '.' + obj_id:
-                del type(self).__objects[key]
-                self.save()
-                return
-
-        raise AttributeError(
-            "'{}' has no instance with id '{}'".format(class_name,
-                                                       obj_id))
-
-    def validate_id(self, class_name, obj_id):
-        """Check if a valid object id was provided"""
-        if class_name + '.' + obj_id not in type(self).__objects.keys():
-            return False
-        return True
-
-    def update(self, attr, val, class_name, obj_id):
-        """A function that updates an instance based on its class
-        name and id
-        """
-        for key in type(self).__objects.keys():
-            if key == class_name + '.' + obj_id:
-                type(self).__objects[key][attr] = val
-                type(self).__objects[key]['updated_at'] = datetime.now()
-                self.save()
+    def attributes(self):
+        """Returns the valid attributes and their types for classname"""
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
+        }
+        return attributes
